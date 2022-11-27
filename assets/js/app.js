91993d996f22da49eeaccde5da62a2c1279fd4fc
poweredbynano <nanolinksgh@gmail.com>
2022-11-27 14:16:12 +0000

    Added functions and click events

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,3 +21,18 @@ function setCurrentDbGridSize(newDbGridSize) {
 const colorPicker = document.getElementById('color-picker');
 const colorBtn = document.getElementById('color-btn');
 const rainbowBtn = document.getElementById('rainbow-color-btn');
+const eraserBtn = document.getElementById('eraser-btn');
+const resetBtn = document.getElementById('reset-btn');
+const dbGridSizeValue = document.getElementById('db-size-value');
+const dbGridSizeSlider = document.getElementById('db-size-slider');
+const dbGrid = document.getElementById('db-grid');
+
+colorPicker.oninput = (e) => setCurrentColor(e.target.value);
+colorBtn.onclick = () => setCurrentMode('color');
+rainbowBtn.onclick = () => setCurrentMode('rainbow');
+eraserBtn.onclick = () => setCurrentMode('eraser');
+resetBtn.onclick = () => resetDbGrid();
+dbGridSizeSlider.onmousemove = (e) => updateDbGridSizeValue(e.target.value);
+dbGridSizeSlider.onchange = (e) => changeDbGridSize(e.target.value); 
+
+
