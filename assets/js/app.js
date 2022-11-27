ef9ae7338c66d0bd9d3cc5f07a58efe34dfc4f12
poweredbynano <nanolinksgh@gmail.com>
2022-11-27 14:52:40 +0000

    Added additional functionality to drawing board

--- assets/js/app.js
+++ assets/js/app.js
@@ -36,3 +36,77 @@ dbGridSizeSlider.onmousemove = (e) => updateDbGridSizeValue(e.target.value);
 dbGridSizeSlider.onchange = (e) => changeDbGridSize(e.target.value); 
 
 
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+
+function changeDbGridSize(value) {
+    setCurrentDbGridSize(value);
+    updateDbGridSizeValue(value);
+    resetDbGrid();
+}
+
+function updateDbGridSizeValue(value) {
+    dbGridSizeValue.innerHTML = `${value} x ${value}`;
+}
+
+function resetDbGrid() {
+    clearDbGrid();
+    setupDbGrid(currentDbGridSize);
+}
+
+function clearDbGrid() {
+    dbGrid.innerHTML = '';
+}
+
+function setupDbGrid(size) {
+    dbGrid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    dbGrid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let area = size * size;
+    for(let i = 0; i < area; i++) {
+        const dbGridElement = document.createElement('div');
+        dbGridElement.classList.add('grid_element');
+        dbGridElement.addEventListener('mouseover', changeColor);
+        dbGridElement.addEventListener('mousedown', changeColor);
+        dbGrid.appendChild(dbGridElement);
+    }
+}
+
+function changeColor(e) {
+    if(e.type === 'mouseover' && !mouseDown) return;
+    if(currentMode === 'rainbow') {
+        let randRed = Math.floor(Math.random() * 256);
+        let randGreen = Math.floor(Math.random() * 256);
+        let randBlue = Math.floor(Math.random() * 256);
+        e.target.style.backgroundColor = `rgb(${randRed}, ${randGreen}, ${randBlue})`;
+    }else if(currentMode === 'color') {
+        e.target.style.backgroundColor = currentColor;
+    }else if(currentMode === 'eraser') {
+        e.target.style.backgroundColor = '#fefefe';
+    }
+}
+
+function activateButton(newMode) {
+    if(currentMode === 'rainbow') {
+        rainbowBtn.classList.remove('active');
+    }else if(currentMode === 'color') {
+        colorBtn.classList.remove('active');
+    }else if(currentMode === 'eraser') {
+        eraserBtn.classList.remove('active');
+    }
+
+    if(newMode === 'rainbow') {
+        rainbowBtn.classList.add('active');
+    }else if(newMode === 'color') {
+        colorBtn.classList.add('active');
+    }else if(newMode === 'eraser') {
+        eraserBtn.classList.add('active');
+    }
+}
+
+window.onload = () => {
+    setupDbGrid(defaultDbGridSize);
+    activateButton(defaultMode);
+}
